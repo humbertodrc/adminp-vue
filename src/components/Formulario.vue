9c9d96ebedfbf65aba864d16ac76fa3b1ed7bb52
Humberto Rivero <humbertorivero9@gmail.com>
2023-07-14 12:57:51 -0300

    Feat register form

---
+++ src/components/Formulario.vue
@@ -0,0 +1,31 @@
+<script>
+  
+</script>
+
+<template>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+    <p class="text-lg mt-5 text-center mb-10">Añade pacientes y <span class="text-indigo-600 font-bold">Adminístralos</span></p>
+
+    <!-- Formulario -->
+    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <!-- Nombre -->
+      <div class="mb-5">
+        <label 
+          for="mascota"
+          class="block text-gray-700 uppercase font-bold mb-2"
+        >
+          Nombre Mascota
+        </label>
+        <input 
+          type="text"
+          id="mascota"
+          placeholder="Nombre Mascota"
+          class="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+        >
+      </div>
+    </form>
+  </div>
+</template>
+
+
